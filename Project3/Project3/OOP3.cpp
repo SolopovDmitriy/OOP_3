522b83bf74a8d0e68c6229bb0d4ace179f992df2
Dmitriy Solopov <div.solopov@gmail.com>
2020-05-27 22:37:13 +0300

    Methods:

---
+++ Project3/Project3/OOP3.cpp
@@ -0,0 +1,96 @@
+#include<iostream>
+using namespace std;
+
+//	Inheritance
+//	Polymorphism
+
+class Point
+{
+	double x;
+	double y;
+public:
+	double get_x()const
+	{
+		return x;
+	}
+	double get_y()const
+	{
+		return y;
+	}
+	void set_x(double x)
+	{
+		this->x = x;
+	}
+	void set_y(double y)
+	{
+		this->y = y;
+	}
+	//			Constructors:
+	Point()
+	{
+		cout << "Constructor:\t" << this << endl;
+	}
+	~Point()
+	{
+		cout << "Destructor:\t" << this << endl;
+	}
+
+	//			Methods:
+	double distance(Point other)
+	{
+		double x_distance = this->x - other.x;
+		double y_distance = this->y - other.y;
+		double distance = sqrt(x_distance*x_distance + y_distance * y_distance);
+		return distance;
+	}
+};
+
+double distance(Point A, Point B)
+{
+	double x_distance = A.get_x() - B.get_x();
+	double y_distance = A.get_y() - B.get_y();
+	double distance = sqrt(x_distance*x_distance + y_distance * y_distance);
+	return distance;
+}
+
+class Point3D :public Point
+{
+	double z;
+public:
+	double get_z() const
+	{
+		return z;
+	}
+	void set_z(double z)
+	{
+		this->z = z;
+	}
+
+
+};
+
+
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int a;	//Объявление переменной 'a', типа 'int'.
+	Point A;//Объявление переменной 'A', типа 'Point'.
+			//Создание экземпляра 'A', структуры 'Point'.
+
+	A.set_x(2);
+	A.set_y(3);
+	cout << A.get_x() << "\t" << A.get_y() << endl;
+
+	Point* pA = &A;
+	cout << pA->get_x() << "\t" << pA->get_y() << endl;
+
+	Point B;
+	B.set_x(4);
+	B.set_y(5);
+	cout << "Расстояние от точки 'A' до точки 'B':" << A.distance(B) << endl;
+	cout << "Расстояние от точки 'B' до точки 'A':" << B.distance(A) << endl;
+	cout << "Расстояние между точками 'A' и 'B':  " << distance(A, B) << endl;
+	cout << "Расстояние между точками 'B' и 'A':  " << distance(B, A) << endl;
+
+}
